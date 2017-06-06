6c8c6cc5fbbf96647d2d906227e828fd6aebceb2
dalgim <dalgiewiczmateusz@gmail.com>
2017-06-06 21:57:51 +0200

    Initial commit

---
+++ src/main/java/com/dalgim/example/sb/rest/async/SbRestAsyncApplication.java
@@ -0,0 +1,12 @@
+package com.dalgim.example.sb.rest.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestAsyncApplication.class, args);
+	}
+}
